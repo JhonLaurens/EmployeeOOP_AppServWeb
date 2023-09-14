9d1e3bf1342b0513eb55cdde539f72f0f375c244
Jhon <jhonlaurens@gmail.com>
2023-09-14 18:40:02 -0500

    New abstrac class Emplyee

---
+++ EmployeeOOP/Classes/Employee.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeOOP.Classes
+{
+    internal class Employee
+    {
+
+        #region Properties
+        public int Id { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; } 
+        public Date? BirthDate { get; set; }
+        public Date? HiringDate { get; set; }
+        public bool? IsAcvtive { get; set; }
+
+        #endregion
+
+        #region Methods 
+
+        public abstract decimal GetValueToPay();
+
+        public override string ToString()
+        {
+            return $"INFORMACION EMPLEADO \n\t"+
+                $"ID: {Id}\n\t"+
+                $"Nombres: {Id}\n\t" +
+                $"Apellidos: {Id}\n\t" +
+                $"Fecha de Cumpleaños: {Id}\n\t" +
+                $"Fecha de Contratación: {Id}\n\t" +
+                $"ID: {Id}\n\t" +
+        }
+
+        #endregion
+
+    }
+}
